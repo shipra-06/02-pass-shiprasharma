6fc1216e63c4d4a054578ab941876b1f58d6896b
shipra-06 <shipra.mishra2008@gmail.com>
2021-03-31 13:14:00 +0200

    Added the project

---
+++ first-project/src/main/java/com/assigment/maven/first_project/AdvanceOperation.java
@@ -0,0 +1,11 @@
+package com.assigment.maven.first_project;
+
+public interface AdvanceOperation {
+	
+	public double square(double firstnumber);
+	public double root(double firstnumber);
+	public double cuberoot(double firstnumber);
+	public double cube(double firstnumber);
+	public double log(double firstnumber);
+	
+}
