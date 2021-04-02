da116bd43ec9e72e7d64e20f0c6cbf1497157426
shipra-06 <shipra.mishra2008@gmail.com>
2021-04-02 12:12:43 +0200

    Add refactored test cases.

--- first-project/src/test/java/com/assigment/maven/first_project/AdvanceCalculatorTest.java
+++ first-project/src/test/java/com/assigment/maven/first_project/AdvanceCalculatorTest.java
@@ -3,30 +3,168 @@ package com.assigment.maven.first_project;
 
 	import static org.junit.Assert.assertEquals;
 
-	import org.junit.Test;
+import java.util.Random;
+import java.util.logging.Logger;
+
+import org.junit.Test;
 
 	public class AdvanceCalculatorTest {
 		AdvanceCalculator ac = new AdvanceCalculator();
 		
+		public static final Logger LOGGER =  Logger.getLogger(AdvanceCalculatorTest.class.getName());
+		
+		
+		Random random = new Random();
+		
 		
 		@Test
-		public void squareTest() {
-			assertEquals(ac.square(4), 16,0);
+		public void positivesquareTest() {
+				for (int i = 0; i < 10; i++) {
+					double tempValue1 = random.nextDouble()*1;
+					double tempResult = tempValue1*tempValue1;
+					LOGGER.info("Testing square with  number 1:: "+ tempValue1);
+					assertEquals(ac.square(tempValue1), tempResult,0);
+				}
+				
+			
+		}
+		
+		@Test
+		public void negativesquareTest() {
+				for (int i = 0; i < 10; i++) {
+					double tempValue1 = random.nextDouble()*(-1);
+					double tempResult = tempValue1*tempValue1;
+					LOGGER.info("Testing square with  number 1:: "+ tempValue1);
+					assertEquals(ac.square(tempValue1), tempResult,0);
+				}
 		}
 		@Test
-		public void rootTest() {
-			assertEquals(ac.root(16), 4,0);
+		public void zerosquareTest() {
+				for (int i = 0; i < 10; i++) {
+					double tempValue1 = 0;
+					double tempResult = 0;
+					LOGGER.info("Testing square with  number 1:: "+ tempValue1);
+					assertEquals(ac.square(tempValue1), tempResult,0);
+				}
+		}
+				
+				
+				
+		@Test
+		public void positiverootTest() {
+			for (int i = 0; i < 10; i++) {
+				double tempValue1 = random.nextDouble()*10;
+				double tempResult = Math.sqrt(tempValue1);
+				LOGGER.info("Testing root with  number 1:: "+ tempValue1);
+				assertEquals(ac.root(tempValue1), tempResult,0);
+			}
+		}
+		
+		@Test
+		public void negativerootTest() {
+				for (int i = 0; i < 10; i++) {
+					double tempValue1 = random.nextDouble()*(-10);
+					double tempResult = Math.sqrt(tempValue1);
+					LOGGER.info("Testing root with  number 1:: "+ tempValue1);
+					assertEquals(ac.root(tempValue1), tempResult,0);
+				}
+		}
+		
+		@Test
+		public void zerorootTest() {
+				for (int i = 0; i < 10; i++) {
+					double tempValue1 = 0;
+					double tempResult = 0;
+					LOGGER.info("Testing root with  number 1:: "+ tempValue1);
+					assertEquals(ac.root(tempValue1), tempResult,0);
+				}
 		}
 		@Test
-		public void cuberootTest() {
-			assertEquals(ac.cuberoot(125), 5,0);
+		public void positivecuberootTest() {
+			for (int i = 0; i < 10; i++) {
+				double tempValue1 = random.nextDouble()*(10);
+				double tempResult = Math.cbrt(tempValue1);
+				LOGGER.info("Testing cuberoot with  number 1:: "+ tempValue1);
+				assertEquals(ac.cuberoot(tempValue1), tempResult,0);
+			}
+		}
+		
+		@Test
+		public void negativecuberootTest() {
+				for (int i = 0; i < 10; i++) {
+					double tempValue1 = random.nextDouble()*(-10);
+					double tempResult = Math.cbrt(tempValue1);
+					LOGGER.info("Testing cuberoot with  number 1:: "+ tempValue1);
+					assertEquals(ac.cuberoot(tempValue1), tempResult,0);
+				}
+		}
+		
+		@Test
+		public void zerocuberootTest() {
+				for (int i = 0; i < 10; i++) {
+					double tempValue1 = 0;
+					double tempResult = 0;
+					LOGGER.info("Testing cuberoot with  number 1:: "+ tempValue1);
+					assertEquals(ac.cuberoot(tempValue1), tempResult,0);
+				}
+		}
+		
+		@Test
+		public void positivecubeTest() {
+			for (int i = 0; i < 10; i++) {
+				double tempValue1 = random.nextDouble()*(10);
+				double tempResult = tempValue1*tempValue1*tempValue1;
+				LOGGER.info("Testing cube with  number 1:: "+ tempValue1);
+				assertEquals(ac.cube(tempValue1), tempResult,0);
+			}
+		}
+		
+		@Test
+		public void negativecubeTest() {
+				for (int i = 0; i < 10; i++) {
+					double tempValue1 = random.nextDouble()*(-10);
+					double tempResult = tempValue1*tempValue1*tempValue1;
+					LOGGER.info("Testing cube with  number 1:: "+ tempValue1);
+					assertEquals(ac.cube(tempValue1), tempResult,0);
+				}
+		}
+		
+		@Test
+		public void zerocubeTest() {
+				for (int i = 0; i < 10; i++) {
+					double tempValue1 = 0;
+					double tempResult = 0;
+					LOGGER.info("Testing cube with  number 1:: "+ tempValue1);
+					assertEquals(ac.cube(tempValue1), tempResult,0);
+				}
 		}
 		@Test
-		public void cubeTest() {
-			assertEquals(ac.cube(5),125,0);
+		public void positivelogTest() {
+			for (int i = 0; i < 10; i++) {
+				double tempValue1 = random.nextDouble()*(10);
+				double tempResult = Math.log(tempValue1);
+				LOGGER.info("Testing Log with  number 1:: "+ tempValue1);
+				assertEquals(ac.log(tempValue1), tempResult,0);
+			}
 		}
 		@Test
-			public void logTest() {
-				assertEquals(ac.cube(5),125,0);
+		public void negativelogTest() {
+				for (int i = 0; i < 10; i++) {
+					double tempValue1 = random.nextDouble()*(-10);
+					double tempResult = Math.log(tempValue1);
+					LOGGER.info("Testing Log with  number 1:: "+ tempValue1);
+					assertEquals(ac.log(tempValue1), tempResult,0);
+				}
 		}
-}
+		
+		@Test
+		public void zerologTest() {
+				for (int i = 0; i < 10; i++) {
+					double tempValue1 = 0;
+					double tempResult = Math.log(tempValue1);
+					LOGGER.info("Testing Log with  number 1:: "+ tempValue1);
+					assertEquals(ac.log(tempValue1), tempResult,0);
+				}
+		}
+	}
+		
--- first-project/src/test/java/com/assigment/maven/first_project/BasicCalculatorTest.java
+++ first-project/src/test/java/com/assigment/maven/first_project/BasicCalculatorTest.java
@@ -2,27 +2,163 @@ package com.assigment.maven.first_project;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Random;
+import java.util.logging.Logger;
+
 import org.junit.Test;
 
 public class BasicCalculatorTest {
+	
+	public static final Logger LOGGER =  Logger.getLogger(BasicCalculatorTest.class.getName());
+	
 	BasicCalculator bc = new BasicCalculator();
+	Random random = new Random();
 	
+	@Test
+	public void positiveadditionTest() {
+		for (int i = 0; i < 10; i++) {
+			double tempValue1 = random.nextDouble()*10;
+			double tempValue2 = random.nextDouble()*10;
+			double tempResult = tempValue1 + tempValue2;
+			LOGGER.info("Testing addition with  number 1:: "+ tempValue1);
+			LOGGER.info(" Testing addition with number 2:: "+ tempValue2);
+			assertEquals(bc.addition(tempValue1,tempValue2), tempResult,0);
+		}
+		
+	}
+	@Test
+	public void negativeadditionTest() {
+		for (int i = 0; i < 10; i++) {
+			double tempValue1 = random.nextDouble()*(-10);
+			double tempValue2 = random.nextDouble()*(-10);
+			double tempResult = tempValue1 + tempValue2;
+			LOGGER.info("Testing addition with  number 1:: "+ tempValue1);
+			LOGGER.info(" Testing addition with number 2:: "+ tempValue2);
+			assertEquals(bc.addition(tempValue1,tempValue2), tempResult,0);
+		}
+	}
+	@Test
+	public void ZeroadditionTest() {
+		for (int i = 0; i < 10; i++) {
+			double tempValue1 = 0;
+			double tempValue2 = 0;
+			double tempResult = tempValue1 + tempValue2;
+			LOGGER.info("Testing addition with  number 1:: "+ tempValue1);
+			LOGGER.info(" Testing addition with number 2:: "+ tempValue2);
+			assertEquals(bc.addition(tempValue1,tempValue2), tempResult,0);
+		}
+	}
+		
+	@Test
+	public void positivesubstractionTest() {
+		for (int i = 0; i < 10; i++) {
+			double tempValue1 = random.nextDouble()*10;
+			double tempValue2 = random.nextDouble()*10;
+			double tempResult = tempValue1 - tempValue2;
+			LOGGER.info("Testing substraction with  number 1:: "+ tempValue1);
+			LOGGER.info(" Testing substraction with number 2:: "+ tempValue2);
+			assertEquals(bc.substraction(tempValue1,tempValue2), tempResult,0);
+		}
+			
+	
+	}
+	@Test
+	public void negativesubstractionTest() {
+		for (int i = 0; i < 10; i++) {
+			double tempValue1 = random.nextDouble()*(-10);
+			double tempValue2 = random.nextDouble()*(-10);
+			double tempResult = tempValue1 - tempValue2;
+			LOGGER.info("Testing substraction with  number 1:: "+ tempValue1);
+			LOGGER.info(" Testing substraction with number 2:: "+ tempValue2);
+			assertEquals(bc.substraction(tempValue1,tempValue2), tempResult,0);
+		}
+			
+	}
+	@Test
+	public void zerosubstractionTest() {
+		for (int i = 0; i < 10; i++) {
+			double tempValue1 = 0;
+			double tempValue2 = 0;
+			double tempResult = tempValue1 - tempValue2;
+			LOGGER.info("Testing substraction with  number 1:: "+ tempValue1);
+			LOGGER.info(" Testing substraction with number 2:: "+ tempValue2);
+			assertEquals(bc.substraction(tempValue1,tempValue2), tempResult,0);
+		}
+			
+	
+	}
+	@Test
+	public void positivemultiplicationTest() {
+		for (int i = 0; i < 10; i++) {
+		double tempValue1 = random.nextDouble()*10;
+		double tempValue2 = random.nextDouble()*10;
+		double tempResult = tempValue1 * tempValue2;
+		LOGGER.info("Testing multiplication with  number 1:: "+ tempValue1);
+		LOGGER.info(" Testing multiplication with number 2:: "+ tempValue2);
+		assertEquals(bc.multiplication(tempValue1,tempValue2), tempResult,0);
+	}
+
+	}
+	@Test
+	public void neagtivemultiplicationTest() {
+		for (int i = 0; i < 10; i++) {
+		double tempValue1 = random.nextDouble()*(-10);
+		double tempValue2 = random.nextDouble()*(-10);
+		double tempResult = tempValue1 * tempValue2;
+		LOGGER.info("Testing multiplication with  number 1:: "+ tempValue1);
+		LOGGER.info(" Testing multiplication with number 2:: "+ tempValue2);
+		assertEquals(bc.multiplication(tempValue1,tempValue2), tempResult,0);
+	}
+
+	}
+	@Test
+	public void zeromultiplicationTest() {
+		for (int i = 0; i < 10; i++) {
+		double tempValue1 = 0;
+		double tempValue2 = 0;
+		double tempResult = tempValue1 * tempValue2;
+		LOGGER.info("Testing multiplication with  number 1:: "+ tempValue1);
+		LOGGER.info(" Testing multiplication with number 2:: "+ tempValue2);
+		assertEquals(bc.multiplication(tempValue1,tempValue2), tempResult,0);
+	}
+
+	}
 	
 	@Test
-	public void additionTest() {
-		assertEquals(bc.addition(2.2,8.1), 10.3,0);
+	public void positivedivisionTest() {
+		for (int i = 0; i < 10; i++) {
+			double tempValue1 = random.nextDouble()*10;
+			double tempValue2 = random.nextDouble()*10;
+			double tempResult = tempValue1/tempValue2;
+			LOGGER.info("Testing Division with  number 1:: "+ tempValue1);
+			LOGGER.info(" Testing Division with number 2:: "+ tempValue2);
+			assertEquals(bc.division(tempValue1,tempValue2), tempResult,0);
+		}
+		
 	}
 	@Test
-	public void substractionTest() {
-		assertEquals(bc.substraction(8.1,8.1), 0.0,0);
+	public void negativedivisionTest() {
+		for (int i = 0; i < 10; i++) {
+			double tempValue1 = random.nextDouble()*(-10);
+			double tempValue2 = random.nextDouble()*(-10);
+			double tempResult = tempValue1/tempValue2;
+			LOGGER.info("Testing Division with  number 1:: "+ tempValue1);
+			LOGGER.info(" Testing Division with number 2:: "+ tempValue2);
+			assertEquals(bc.division(tempValue1,tempValue2), tempResult,0);
+		}
+		
 	}
 	@Test
-	public void multiplicationTest() {
-		assertEquals(bc.multiplication(4, 3), 12,0);
-	}
-	@Test
-	public void divisionTest() {
-		assertEquals(bc.division(5, 5), 1,0);
+	public void zeropositivedivisionTest() {
+		for (int i = 0; i < 10; i++) {
+			double tempValue1 = 0;
+			double tempValue2 = 0;
+			double tempResult = tempValue1/tempValue2;
+			LOGGER.info("Testing Division with  number 1:: "+ tempValue1);
+			LOGGER.info(" Testing Division with number 2:: "+ tempValue2);
+			assertEquals(bc.division(tempValue1,tempValue2), tempResult,0);
+		}
+		
 	}
 	
 
