840d7e369c7c67c5d2c2e3bda87425060bfe7c5a
shipra-06 <shipra.mishra2008@gmail.com>
2021-04-30 22:42:41 +0200

    refactored the repository

---
+++ first-project/src/main/java/com/assigment/maven/first_project/TestExample.java
@@ -0,0 +1,14 @@
+package com.assigment.maven.first_project;
+
+public class TestExample {
+	
+	public int add1(int value) {
+		return value+1;
+	}
+	public int add2(int value) {
+		return value+2;
+	}
+	public int add3(int value) {
+		return value+3;
+	}
+}
