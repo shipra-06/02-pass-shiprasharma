9e3ab3d648f6f2029c5f085fca2079bf66a72d60
shipra-06 <shipra.mishra2008@gmail.com>
2021-04-30 22:17:06 +0200

    fixing the merging

--- first-project/src/main/java/com/assigment/maven/first_project/BasicOperation.java
+++
@@ -1,9 +0,0 @@
-package com.assigment.maven.first_project;
-
-public interface BasicOperation {
-	public double addition(double firstnumber, double secondnumber);
-	public double substraction(double firstnumber, double secondnumber);
-	public double multiplication(double firstnumber, double secondnumber);
-	public double division(double firstnumber, double secondnumber);
-
-}
